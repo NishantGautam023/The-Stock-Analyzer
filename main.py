1e0eb1e533719587f20aecb5630bd2ee3b446028
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-06-07 21:52:25 -0400

    update settings.py to load from dotenv

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import openai
 from flask import Flask, render_template
-import os
+
 
 
 app  = Flask(__name__)
 app.config.from_object("settings")
+
+openai.api_key = app.config['OPENAI_API_KEY']
 
 
 @app.route("/", methods=["GET", "POST"])
 def render_home():
     return render_template('index.html')
 
+
+@app.route('/ask-ai')
+def ask_openai():
+    # Todo: Learn how to create assistants and Thread Objects: https://platform.openai.com/docs/assistants/overview
+    return "<h1>OpenAI Loading ... </h>1"
 
 if __name__ == '__main__':
     app.run(
--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 
-HOST = '127.0.0.1'
-PORT = 8848
-debug = True
+HOST = os.getenv('HOST', '127.0.0.1')
+PORT = int(os.getenv('PORT', 8848))
+debug = os.getenv('DEBUG', 'False') == 'True'
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
